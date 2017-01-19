c4b3e7b75b2395701d5a4f96f9071e450c23b9c2
Jacob Hedengren <=>
2017-01-19 21:24:06 +0100

    generated about, contact and for_employers index action and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  resources :contact
+  resources :for_employers
+  resources :about
+
   root 'landing#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
